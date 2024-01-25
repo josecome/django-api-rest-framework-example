fb682d5ac97527c2eb648ad188fd815e0a9452df
Jose Jaime Come <josejaimecome@gmail.com>
2024-01-25 20:33:28 +0200

    Minor changes made

--- django_api_rest_framework_example/urls.py
+++ django_api_rest_framework_example/urls.py
@@ -19,7 +19,8 @@ from django.conf.urls import include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView # To verify Token
+    TokenVerifyView,
+    TokenBlacklistView
 )
 
 urlpatterns = [
@@ -27,5 +28,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('api/', include('api.urls')),
 ]
